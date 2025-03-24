1569b9206d0c9c56fd6e50f7c5dd2ee0c923b069
Summer <65177206+DaftStrike@users.noreply.github.com>
2025-03-23 21:25:37 -0300

    Verificador de Pares.c

--- Verificador de Pares.c
+++ Verificador de Pares.c
@@ -1,42 +1,20 @@
-/*Programa de teste
+/*
+	Algoritmo capaz de verificar o status de um numero.
 	Autor: Pedro Vnzella
-	Data: 22/02/2025
+	Data: 21/03/2025
 */
 
 #include <stdio.h>
-#include <string.h>
-#define TAM 50
 
 int main(void)
 {
-	//Variaveis
-	char nome[TAM];
-	char genero[TAM];
-	int idade;
-	int salario;
+	int Num;
 	
-	//Atribuicoes
-	printf("Digite o seu nome:\n");
-	fgets(nome, TAM, stdin);
-	nome[strlen(nome) - 1] = '\0';
-	
-	printf("Digite o seu genero:\n");
-	fgets(genero, TAM, stdin);
-	genero[strlen(genero) - 1] = '\0';
-	
-	printf("Digite a sua idade:\n");
-	scanf("%d", &idade);
-	
-	printf("Quanto planeja receber? (Pergunta eliminatoria)\n");
-	scanf("%d", &salario);
-	
-	printf("\n");
-	
-	//Impressao
-	printf("Nome: %s\n", nome);
-	printf("Genero: %s\n", genero);
-	printf("Idade: %d\n", idade);
-	printf("Salario Desejado: R$%d\n\n", salario);
-	printf("Vaga criada com sucesso!");
+	printf("Digite um numero inteiro:\n");
+	scanf("%d", &Num);
+	if(Num % 2 == 0)
+		printf("O numero eh par.");
+	else
+		printf("O numero eh impar.");
 	return 0;
 }
